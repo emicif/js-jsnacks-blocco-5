5e9551db010f28328a883367106f966bdb0a7176
emicif <emanuela.cifolelli@gmail.com>
2022-03-22 16:43:00 +0100

    snack3

--- snack3/script.js
+++ snack3/script.js
@@ -2,3 +2,13 @@ console.log('JS OK!')
 
 //Scrivi una funzione che accetti una stringa come argomento 
 // e la ritorni girata (es. Ciao -> oaiC)
+
+stringa('ciao');
+
+function stringa (parola) {
+    const parolaDivisa = parola.split('');
+    const parolaReverse = parolaDivisa.reverse();
+    const parolaInvertita = parolaReverse.join('');
+    console.log('La parola invertita è', parolaInvertita);
+}
+
